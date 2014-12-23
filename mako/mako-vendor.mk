5f2b0a799f17d1d5ed9ae4e4c05391963c372a5c
Tom Marshall <tdm.code@gmail.com>
2014-12-23 16:39:29 +0100

    mako: Update blobs to LRX21T
    Change-Id: Ibaadbea4831ceb26fe7947b54a1ce7989cb3d856

--- mako/mako-vendor.mk
+++ mako/mako-vendor.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2013 The CyanogenMod Project
+# Copyright (C) 2014 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,7 +16,5 @@
 
 # Pick up overlay for features that depend on non-open-source files
 DEVICE_PACKAGE_OVERLAYS := vendor/lge/mako/overlay
-
-PRODUCT_PACKAGES += libacdbloader
 
 $(call inherit-product, vendor/lge/mako/mako-vendor-blobs.mk)
